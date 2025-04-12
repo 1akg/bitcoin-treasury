c31eaba4c9ead3ed27620980c9543198a2d6a376
1akg <40740054+1akg@users.noreply.github.com>
2025-04-11 23:48:54 -0400

    Fix syntax error in page.tsx for successful deploy

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,6 @@ const address = "bc1q6920ksguf766wye08azalqfk9f426nqn3xx73r";
 const btcBalance = (balanceData.chain_stats.funded_txo_sum - balanceData.chain_stats.spent_txo_sum) / 1e8;
 setHolding(btcBalance);
 
-holding={holding.toFixed(5)}
+// holding={holding.toFixed(5)} // This line has been removed
 
 module.exports = nextConfig;
